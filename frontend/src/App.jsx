66c0bb37c00e5c8d7c3db539ccf2aa136f66d39f
adarsh gurjar <adarsh7470827890@gmail.com>
2024-03-12 14:43:24 +0530

    adding suspense

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { useSelector } from 'react-redux'
-import About from './pages/About'
-import Dashboard from './Dashboard/Dashboard'
-import Login from './NavPages/Login'
 import Home from './pages/Home'
-const SignUp = React.lazy(()=>import('./NavPages/SignUp')) 
 import MyHome from './pages/MyHome'
-import Contact from './pages/Contact'
-import CreatePost from './Components/CreatePost'
 import UpdatePost from './Components/UpdatePost'
 import PostSeen from './Components/PostSeen'
-import SearchPage from './Components/SearchPage'
+const About = React.lazy(()=>import('./pages/About'))
+const Dashboard = React.lazy(()=>import('./Dashboard/Dashboard'))
+const Login = React.lazy(()=>import('./NavPages/Login')) 
+const SignUp = React.lazy(()=>import('./NavPages/SignUp')) 
+const Contact = React.lazy(()=>import('./pages/Contact'))
+const CreatePost = React.lazy(()=>import('./Components/CreatePost'))
+const SearchPage = React.lazy(()=>import('./Components/SearchPage'))
 
 const queryClient = new QueryClient({
   defaultOptions :{
@@ -41,12 +41,12 @@ const router  = createBrowserRouter([{
       },
       {
           path:'/dashboard',
-          element: user ? <Dashboard/> : <Login/> ,
+          element: user ? <Dashboard/> :<Suspense fallback = {<div>Loading..</div>}><Login/></Suspense>  ,
           
      },
      {
          path:'/create-post',
-         element: user && user.isAdmin ? <CreatePost/> : <MyHome/>
+         element: user && user.isAdmin ?<Suspense fallback = {<div>Loading..</div>}><CreatePost/></Suspense>  : <MyHome/>
      },
      {
         path:'/search',
@@ -54,7 +54,7 @@ const router  = createBrowserRouter([{
      },
      {
       path:'/update-post/:id',
-      element: user && user.isAdmin ? <UpdatePost/> : <MyHome/>
+      element: user && user.isAdmin ?<Suspense fallback = {<div>Loading..</div>}><UpdatePost/></Suspense>  : <MyHome/>
   },
   {
    path:'/post/:slug',
@@ -62,12 +62,12 @@ const router  = createBrowserRouter([{
   },
     {
     path:'/about',
-    element:<About/>
+    element:<Suspense fallback = {<div>Loading..</div>}><About/></Suspense>
     },
    
       {
  path:'/login',
- element:<Login/>
+ element:<Suspense fallback = {<div>Loading..</div>}><Login/></Suspense>
     }
     ,
     {
@@ -76,7 +76,7 @@ const router  = createBrowserRouter([{
            
     },{
       path:'/contact-us',
-      element:<Contact/>
+      element:<Suspense fallback = {<div>Loading..</div>}><Contact/></Suspense>
     }]
 }])
 
